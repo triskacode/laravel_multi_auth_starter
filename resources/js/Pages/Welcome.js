8ef40bfbf859904cd56d1498a6679e3523e547b4
Triska Mahfud K <triska120502@gmail.com>
2021-09-14 01:53:40 +0700

    configure database and starting a project

--- resources/js/Pages/Welcome.js
+++ resources/js/Pages/Welcome.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, Head } from '@inertiajs/inertia-react';
 
 export default function Welcome(props) {
+    useEffect(() => { console.log(props, route()) }, [])
     return (
         <>
             <Head title="Welcome" />
